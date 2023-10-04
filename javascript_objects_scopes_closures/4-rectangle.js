ea53f2473d50a50dfbe4a33c6cb9aa7c6fad86cd
Olufemi Odunlami <femi9naija@gmail.com>
2023-10-05 00:08:12 +0100

    4. Rectangle #4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+module.exports =
+class Rectangle {
+    constructor(w, h) {
+        if(w <= 0 || h <= 0 || isNaN(w) || isNaN (h)) {
+            return;
+        }
+        this.width = w;
+        this.height = h;
+    }
+
+    print() {
+        for(let i = 0; i < this.height; i++) {
+            console.log("X".repeat(this.width));
+        }
+    }
+
+    rotate() {
+        let temp;
+        temp = this.width;
+        this.width = this.height;
+        this.height = temp;
+    }
+
+    double() {
+        this.width = this.width * 2;
+        this.height = this.height * 2;
+    }
+}
