c1a47cab22d020ffa1b83c13eefb617acf7cdcda
Olufemi Odunlami <femi9naija@gmail.com>
2023-10-05 00:23:47 +0100

    5. Square #0

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 const Rectangle = require('./4-rectangle');
 
-module.exports =
+// module.exports =
 class Square extends Rectangle {
     constructor(size) {
         super(size, size);
-        this.size = size;
     }
 }
+
+const s1 = new Square(4);
+console.log(s1);
+console.log(s1.width);
+console.log(s1.height);
