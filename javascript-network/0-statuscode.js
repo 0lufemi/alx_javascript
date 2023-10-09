b0f22b91af9ae25ccc36f8a77a815b19b3e69c98
Olufemi Odunlami <femi9naija@gmail.com>
2023-10-09 14:14:39 +0100

    0. Status code

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -5,5 +5,5 @@ const process = require('process');
 const url = process.argv[2];
 
 request.get(url, function (error, response, body) {
-  console.log('code: ', response.statusCode);
+  console.log('code:', response.statusCode);
 });
